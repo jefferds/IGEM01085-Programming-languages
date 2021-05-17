319431a231b559052b0be8e9cd5d8a1cbddb1897
jefferds <jefferds@gmail.com>
2021-05-17 18:28:02 +0000

    Deploying to gh-pages from  @ ae0479dd1917450673194f3556e2434cf2c048b0 🚀

---
+++ Plano Unidade 01 - Apresentação.jl
@@ -0,0 +1,112 @@
+### A Pluto.jl notebook ###
+# v0.14.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano da Unidade 01 de Linguagens de programação - Conceitos Básicos.
+
+## Ementa da Disciplina:
+
+### Tema relacionado na ementa:
+
+1. Conceitos primários: Algoritmo, programa, linguagem de programação, compilador. 
+
+### Subitens
+
+**Fonte:** ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. Fundamentos da programação de computadores. Pearson Educación, 2008.
+
+1.1 Conceito de algoritmo
+
+1.2 Método para a construção de algoritmos
+
+1.3 Tipos de algoritmos
+
+1.4 Exemplos de algoritmos - Algorítmo para cálculo da posição de um projetil (Lançamento oblíquo). 
+
+## Resultados de aprendizagem esperados
+
+✔ Compreendeer e aplicar os conceitos de algoritmo, programa e linguagem de programação.
+
+✔ Apresentar a modelagem para cálculo das equações de lançamento oblíquo. 
+
+✔ Utilizar as ferramentas $(html"<a href='https://code.visualstudio.com/' target='_blank'>Vscode</a>") e $(html"<a href='https://julialang.org/' target='_blank'>Linguagem Julia</a>") e $(html"<a href='https://github.com/fonsp/Pluto.jl' target='_blank'>Pluto.jl</a>") para simulações e apresentação de relatórios.
+
+✔ Discutir duas tarefas entre as principais dos engenheiros, a saber, simulações e cálculos.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais. 
+
+✔ Trabalhar em equipe. 
+
+✔ Comunicar-se oralmente. 
+
+✔ Ter responsabilidade coletiva.
+
+
+# Roteiro da Unidade
+
+#### Momento Pré
+
+🕐 1. O professor anuncia por meio de uma orientação por escrito (Sala):
+
+- A atividade a ser desenvolvida:
+
+	Implementar em liguagem Julia, utilizando vscode e Pluto.jl, uma modelo computacional que permina calcular a posição de um projétil em lançamento oblíquo. 
+
+	Na produção e apresentação do relatório discutir, os conceitos de Algoritmo, programa, linguagem de programação e compilador.
+
+🕑  2. (60 min) Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de até uma página do que estudou (Resumo pré);
+
+#### Bibliografia.
+
+* ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. Fundamentos da programação de computadores. Pearson Educación, 2008.
+
+* Pesquisa a rede computadores. [As fontes devem ser formalmente citadas na norma da ABNT](http://www.biblioteca.fsp.usp.br/~biblioteca/guia/a_modelos.htm).
+
+
+# Roteiro da aula
+
+#### Momento Aula
+
+🕒 3. (10 min) Rápida apresentação do tema pelo professor.
+
+🕓 4. (15 min) Promoção de discussão sobre os interesses no tema que será estudado. O que querem aprender e experienciar durante a unidade? (Sala).
+
+🕔 5. (5 min) Seleção do tópico que cada grupo irá estudar. Os estudantes discutem seus interesses e selecionam um tópico para seu grupo (Sala).
+
+🕕 6. (5 min) Cada grupo divide seu tópico em subtópicos para dividir o trabalho dentro do grupo (Sala).
+
+🕖 7. (30 min) Preparação dos subtópicos pelos membros do grupo (estudo individual).
+
+🕗 8. (10 min) Os membros do grupo discutem o tema, como um painel de especialistas, apresentando os conceitos e questões chaves (reunião do grupo).
+
+🕘 9. (10 min) Apresentação formais dos subtópicos em cada grupo (reunião do grupo).
+
+🕙 10. (5 min/grupo) Apresentações de subtópicos pelos grupos (seleção por sorteio a critério do professsor) (sala).
+
+## Roteiro
+
+#### Momento Pós
+
+🕚 11. (30 min) Preparação das apresentações de cada um dos grupos (reunião do grupo).
+
+🕛 12. (10 min) Envio via Classroom do relatório da atividade para postagem na internet (atividade do grupo).
+
+🕐 13. (10 min/grupo) Apresentações de cada um dos grupos (sala).
+
+🕑 14. (20 min/grupo) Avaliações (sala ou extra sala).
+
+🕒 15. (10 min/grupo) Feedback (sala).
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
