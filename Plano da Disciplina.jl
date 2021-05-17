99f63765b4cf16ce83718961ac03daf52887eb1d
jefferds <jefferds@gmail.com>
2021-05-17 12:11:05 -0300

    Inserir plano da disciplina

---
+++ Plano da Disciplina.jl
@@ -0,0 +1,219 @@
+### A Pluto.jl notebook ###
+# v0.14.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e6e24892-e2b7-472f-9a5c-7d1cbe34eeb6
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ f92f9d30-b368-11eb-2e66-a7bbf5a4a943
+md"""
+# Plano da disciplina de Linguagens de Programação
+
+## Ementa da Disciplina:
+
+1. Conceitos primários: 
+
+	- Algoritmo, programa, linguagem de programação, compilador. 
+
+
+	- Evolução das linguagens de programação. 
+
+2. Desenvolvimento de algoritmos: linguagens para algoritmos e refinamento passo a passo. 
+
+3. Comandos de uma linguagem procedimental: atribuição, entrada e saída, condicionais, repetitivos e seletivos. 
+
+	- Variáveis escalares e estruturadas homogêneas e heterogêneas. Vetores e encadeamento de estruturas. 
+
+4. Subprogramação: funções, procedimentos, passagem de parâmetros, recursividade.  Ponteiros. 
+
+5. Noções de programação orientada a objetos.
+
+
+## Resultados de aprendizagem esperados
+
+##### 🥇 Utilizar computadores e softwares no desenvolvimento de atividades de Engenharia Mecânica.
+
+##### 🥈 Desenvolver programas computacionais em linguagens de programações para solução de problema de Engenharia Mecânica.
+
+##### 🥉 Conhecer ferramentas computacionais e aplicativos capazes de auxílio em atividades relacionados à Engenharia.
+
+
+## Metodologia
+
+### Estratégia
+
+Com base no livro de ELMÔR FILHO, Gabriel et al. $(html"<a href='https://g.co/kgs/z62mSU' target='_blank'>Uma nova sala de aula é possível: aprendizagem ativa na educação em engenharia.</a>") Rio de Janeiro: LTC, 2019.
+
+
+Será aplicada a **Sala de aula invertida** com utilização da Estratégia *Co-op co-op* que é uma estratégia cooperativa de aprendizagem ativa.
+
+🧑‍🎓 Os estudantes trabalham juntos, em pequenas equipes (3 ou 4 membros), para investigar um tópico ou resolver um problema, e produzir um produto de grupo que eles compartilham com toda a classe.
+
+🧑‍🎓 Os estudantes cooperam dentro de suas equipes para produzir algo que irá beneficiar o grande grupo (Estudos de caso que ficarão disponíveis na internet até o fim da disciplina).
+
+👨‍🔬 Mediação do professor em todas as etapas de desenvolvimento da estratégia.
+
+## Roteiro
+
+#### Momento Pré
+
+🕐 1. O professor anuncia por meio de uma orientação por escrito (Sala):
+
+   - A atividade a ser desenvolvida.
+   - O respectivo tema e subtópicos.
+   - Uma síntese da estratégia e 
+   - O cronograma incluindo o período para as avaliações da atividade.
+
+🕑 2. Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de uma página do que estudou (Resumo pré) (Extra sala).
+
+## Roteiro
+
+#### Momento Aula
+
+🕒 3. Rápida verificação do entendimento do tema pelo professor (Sala).
+
+🕓 4. Promoção de discussão sobre os interesses no tema que será estudado. O que querem aprender e experienciar durante a unidade? (Sala).
+
+🕔 5. Seleção do tópico que cada grupo irá estudar. Os estudantes discutem seus interesses e selecionam um tópico para seu grupo (Aula).
+
+🕕 6. Cada grupo divide seu tópico em subtópicos para dividir o trabalho dentro do grupo (Aula).
+
+🕖 7. Preparação dos subtópicos pelos membros do grupo (estudo individual).
+
+🕗 8. Os membros do grupo discutem o tema, como um painel de especialistas, apresentando os conceitos e questões chaves (reunião do grupo).
+
+🕘 9. Apresentação formais dos subtópicos em cada grupo (reunião do grupo).
+
+🕙 10. Apresentações de subtópicos pelos grupos (seleção por sorteio a critério do professsor) (sala).
+
+## Roteiro
+
+#### Momento Pós
+
+🕚 11. Preparação das apresentações de cada um dos grupos (reunião do grupo).
+
+🕛 12. Envio via Classroom do relatório da atividade para postagem na internet.
+
+🕐 13. Apresentações de cada um dos grupos (sala).
+
+🕑 14. Avaliações (Extra sala).
+
+🕒 15. Feedback (sala).
+
+
+
+## Recursos e Materiais Didáticos
+
+1. Google Classroom;
+2. Google Meet para encontros;
+3. Jamboard como quadro branco;
+4. Google Forms para formulários e avaliações;
+5. Google Drive para arquivos (Docs, Slides, Planilhas, Desenhos);
+6. Os recurso $(html"<a href='https://code.visualstudio.com/' target='_blank'>Vscode</a>") e $(html"<a href='https://julialang.org/' target='_blank'>Linguagem Julia</a>") e $(html"<a href='https://github.com/fonsp/Pluto.jl' target='_blank'>Pluto.jl</a>") para Relatórios.
+7. Página do professor para registro dos casos.
+
+## Indicações de Referências
+
+### Bibliografia Atual
+
+1. Documentação do [Julia](https://docs.julialang.org/en/v1/)
+
+2. Artigos e Blogs na Internet.
+
+
+### Bibliografia Básica (PPC):
+
+1. CELES, W. et al. Introdução a Estruturas de Dados. Rio de Janeiro: Campus-Elsevier,2004.
+
+2. MOKARZEL, F.C.; SOMA, N.Y. Introdução à Ciência da Computação. Rio de Janeiro: Campus-Elsevier, 2008.
+
+3. FEOFILOFF, P. Algoritmos em Linguagem C. Rio de Janeiro: Campus-Elsevier, 2009.
+
+
+### Bibliografia Complementar  (PPC):
+
+1. DROSDEK, A. Estrutura de Dados e Algoritmos em C++. São Paulo: Thomson,2002.
+
+2. MIZRAHI, V.V. Treinamento em Linguagem C. São Paulo: Pearson, 2008.
+
+3. SALIBA, W. L. C. Técnicas de Programação: uma AbordagemAlgorítmica. São Paulo: Makron, 1992.
+
+4. RUMBAUGH, James et al. Object-oriented modeling and design. Englewood Cliffs: Prentice-hall, 1991.
+
+5. ART, Lew; MAUCH, Holger. Introduction to dynamic programming. Springer Berlin Heidelberg, 2007.
+
+
+## Procedimento de Avaliação da Apredizagem
+
+Avaliação será:
+
+- Contínua 
+- Individual 
+- Por atividade a ser desenvolvida
+
+## Procedimento de Avaliação da Apredizagem
+
+### Regras:
+
+- As avaliações serão feitas via $(html"<a href=https://forms.gle/zMoVQnZ7LbPfZFZX7' target='_blank'>Formulário Eletrônico no Google Forms (Acesse aqui)</a>"), com excessão do Resumo pré que deve ser entregue via Classroom.
+- Só serão aceitos emails da Unifesspa.
+- No caso de duplicidade de avaliação será considerada sempre a última avaliação enviada.
+- Serão consideradas apenas as avaliações realizadas no período estipulado, ou até quando estiverem disponíveis no formulário.
+- Cada avaliação receberá uma pontuação de 0 a 100.
+- A nota para o conceito final será a média das notas das atividades propostas.
+
+## Procedimento de Avaliação da Apredizage
+
+### Composição das Notas
+
+A seguir são enumeradas os itens que serão avaliados.
+
+Para cada atividade serão avaliados:
+
+1. Apresentações dos grupos (peso 1).
+2. Contribuições individuais (peso 1).
+3. Relatório em arquivo html para postagem (peso 1).
+
+
+## Procedimento de Avaliação da Apredizagem
+
+### Composição das Notas
+
+#### 1. Apresentações dos grupos (peso 1)
+
+Avalições que serão consideradas neste item: 
+
+- Avaliação por pares (peso 1)
+- Avaliação do professor (peso 1)
+
+
+## Procedimento de Avaliação da Apredizagem
+
+### Composição das Notas
+
+#### 2. Contribuições individuais (peso 1)
+
+Avaliações que serão consideradas: 
+
+- Avaliação do resumo pré (peso 1)
+- Avaliação por pares (peso 1)
+- Avaliação do professor (peso 1)
+
+
+## Procedimento de Avaliação da Apredizagem
+
+### Composição das Notas
+
+#### 3. Relatório em arquivo html para postagem (peso 1).
+
+- Autoavaliação (peso 1)
+- Avaliação por pares (peso 1)
+- Avaliação do professor (peso 1)
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─e6e24892-e2b7-472f-9a5c-7d1cbe34eeb6
+# ╟─f92f9d30-b368-11eb-2e66-a7bbf5a4a943
