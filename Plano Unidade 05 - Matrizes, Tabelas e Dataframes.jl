615d6be00d5ea195081aeac07e7c21e879edea99
jefferds <jefferds@gmail.com>
2021-07-16 07:17:28 -0300

    Unidade 05

---
+++ Plano Unidade 05 - Matrizes, Tabelas e Dataframes.jl
@@ -0,0 +1,87 @@
+### A Pluto.jl notebook ###
+# v0.14.8
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
+html"<button onclick=present()> Presente </button>"
+
+# ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+md"""
+## Plano Unidade 05 - Matrizes, Tabelas e Dataframes
+
+### Tema relacionado na ementa:
+
+3. Comandos de uma linguagem procedimental: atribuição, entrada e saída, condicionais, repetitivos e seletivos.
+
+- Variáveis escalares e estruturadas homogêneas e heterogêneas. Vetores e encadeamento de estruturas.
+
+### Subitens
+
+**Capítulo 7 - Matriz**
+
+- 7.1 Matriz em algoritmos
+- 7.3 Matriz em C/C++
+- 7.4 Matriz em JAVA
+
+**Fonte:** ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. **Fundamentos da programação de computadores**. Pearson Educación, 2008.
+
+**Working with Data Frames**
+[Working with Data Frames.](https://dataframes.juliadata.org/stable/man/working_with_dataframes/)
+
+## Resultados de aprendizagem esperados
+
+✔ Discutir estruturas de arrays, tabelas e Dataframes.
+
+✔ Desenvolver estratégias para solução de problemas. 
+
+✔ Estabelecer relações interpessoais e trabalhar em equipe, comunicar-se oralmente e ter responsabilidade coletiva.
+
+# Roteiro da Unidade
+
+#### Momento Pré
+
+🕐 1. * Atividade a ser executada por cada grupo (momento aula) **(16/07/2021)**
+ 
+- _**Apresentar a utilização de arrays mutidimensionais (Matrizes, tabelas e Matrizes) na linguagem julia**_.
+
+🕒 2. (30 min) Apresentação do tema pelo professor **(16/07/2021)**.
+
+🕑 3. (90 min) Ler os subitens 7.1, 7.3 e 7.4 e o material indicado das páginas do [Working with Data Frames.](https://dataframes.juliadata.org/stable/man/working_with_dataframes/) e postar no Classroom resumo de até uma página do que estudou (Resumo pré) **(até 20/07/2021)**;
+
+#### Bibliografia.
+
+* ASCENCIO, Ana Fernanda Gomes; DE CAMPOS, Edilene Aparecida Veneruchi. Fundamentos da programação de computadores. Pearson Educación, 2008.
+
+* Pesquisa a rede computadores. [As fontes devem ser formalmente citadas na norma da ABNT](http://www.biblioteca.fsp.usp.br/~biblioteca/guia/a_modelos.htm).
+
+* [Working with Data Frames.](https://dataframes.juliadata.org/stable/man/working_with_dataframes/) 
+
+# Roteiro da aula
+
+#### Momento Aula
+
+🕔 4. (30 min) Seleção dos temas e subtemas com justificativa para que o que será aprendendido e experienciado durante a unidade **(20/07/2021)**.
+
+🕕 5. (60 min) Cada grupo divide os subtemas entre os estudantes do grupo; elaboram os estudos dos subtemas (modelos, algoritmos, códigos, simulações e análises); como um painel de especialistas, apresentam ao grupo os conceitos e questões chaves e apresentações formais dos subtemas em cada grupo (reunião do grupo) **(até 23/07/2021)**.
+
+🕙 6. (10 min/subtema) Apresentações de alguns subtemas pelos membros responsávies em cada grupo (seleção por sorteio a critério do professsor) (sala) **(23/07/2021)**.
+
+## Roteiro
+
+#### Momento Pós
+
+🕚 7. (60 min) Preparação das apresentações de cada um dos grupos e envio do relatório da atividade via classroom (atividade do grupo) **(27/07/2021)**.
+
+🕐 8. (10 min/grupo) Apresentações de cada um dos grupos (sala) **(30/07/2021)**.
+
+🕑 9. (20 min/grupo) Avaliações (sala ou extra sala) **(30/07/2021)**.
+
+🕒 10. (10 min/grupo) Feedback (sala) **(30/07/2021)**.
+
+"""
+
+# ╔═╡ Cell order:
+# ╟─e9af4550-b632-11eb-1d0d-e11b45468706
+# ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
