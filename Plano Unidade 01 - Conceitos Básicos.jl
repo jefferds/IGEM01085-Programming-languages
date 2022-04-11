884a1d21b57a3a82d78133c17e320c653f545f1a
jefferds <jefferds@gmail.com>
2022-04-11 16:44:17 -0300

    Unidade 01

--- Plano Unidade 01 - Conceitos Básicos.jl
+++ Plano Unidade 01 - Conceitos Básicos.jl
@@ -1,23 +1,21 @@
 ### A Pluto.jl notebook ###
-# v0.14.5
+# v0.19.0
 
 using Markdown
 using InteractiveUtils
-
-Alterações:
 
 # ╔═╡ e9af4550-b632-11eb-1d0d-e11b45468706
 html"<button onclick=present()> Presente </button>"
 
 # ╔═╡ 4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
 md"""
-## Plano da Unidade 01 de Linguagens de programação - Conceitos Básicos.
+## Plano da Unidade 01 - Linguagens de programação - Conceitos Básicos.
 
 ## Ementa da Disciplina:
 
 ### Tema relacionado na ementa:
 
-1. Conceitos primários: Algoritmo, programa, linguagem de programação, compilador. 
+1. Conceitos primários: Algoritmo, programa, linguagem de programação, compilador.
 
 ### Subitens
 
@@ -29,42 +27,35 @@ md"""
 
 1.3 Tipos de algoritmos
 
-1.4 Exemplos de algoritmos - Algorítmo para cálculo da posição de um projetil (Lançamento oblíquo). 
-
 ## Resultados de aprendizagem esperados
 
-✔ Compreendeer e aplicar os conceitos de algoritmo, programa e linguagem de programação.
-
-✔ Apresentar a modelagem para cálculo das equações de lançamento oblíquo. 
+✔ Compreendeer e aplicar os conceitos de algoritmo, programa e linguagem de programação. 
 
 ✔ Utilizar as ferramentas $(html"<a href='https://code.visualstudio.com/' target='_blank'>Vscode</a>") e $(html"<a href='https://julialang.org/' target='_blank'>Linguagem Julia</a>") e $(html"<a href='https://github.com/fonsp/Pluto.jl' target='_blank'>Pluto.jl</a>") para simulações e apresentação de relatórios.
 
 ✔ Discutir duas tarefas entre as principais dos engenheiros, a saber, simulações e cálculos.
 
-✔ Desenvolver estratégias para solução de problemas. 
+✔ Desenvolver estratégias para solução de problemas.
 
-✔ Estabelecer relações interpessoais. 
+✔ Estabelecer relações interpessoais.
 
-✔ Trabalhar em equipe. 
+✔ Trabalhar em equipe.
 
-✔ Comunicar-se oralmente. 
+✔ Comunicar-se oralmente.
 
 ✔ Ter responsabilidade coletiva.
-
 
 # Roteiro da Unidade
 
 #### Momento Pré
 
-🕐 1. O professor anuncia por meio de uma orientação por escrito (Sala):
+🕐 1. A atividade a ser desenvolvida:
 
-- A atividade a ser desenvolvida:
+Identificar um problema e implementar  uma modelo computacional que permita o cálculo em linguagem Julia, utilizando vscode e Pluto.jl.
 
-	Implementar em liguagem Julia, utilizando vscode e Pluto.jl, uma modelo computacional que permina calcular a posição de um projétil em lançamento oblíquo. 
+Na produção e apresentação do relatório discutir, os conceitos de Algoritmo, programa, linguagem de programação e compilador.
 
-	Na produção e apresentação do relatório discutir, os conceitos de Algoritmo, programa, linguagem de programação e compilador.
-
-🕑  2. (60 min) Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de até uma página do que estudou (Resumo pré);
+🕑  2. (30 min) Os estudantes têm a tarefa de ler sobre o assunto e postar no Classroom resumo de até uma página do que estudou (Resumo pré);
 
 #### Bibliografia.
 
@@ -72,43 +63,41 @@ md"""
 
 * Pesquisa a rede computadores. [As fontes devem ser formalmente citadas na norma da ABNT](http://www.biblioteca.fsp.usp.br/~biblioteca/guia/a_modelos.htm).
 
-
-# Roteiro da aula
-
-#### Momento Aula
-
 🕒 3. (10 min) Rápida apresentação do tema pelo professor.
 
-🕓 4. (15 min) Promoção de discussão sobre os interesses no tema que será estudado. O que querem aprender e experienciar durante a unidade? (Sala).
+🕓 4. (15 min) Promoção de discussão sobre os interesses no tema que será estudado. O que querem aprender e experienciar durante a unidade?
 
-🕔 5. (5 min) Seleção do tópico que cada grupo irá estudar. Os estudantes discutem seus interesses e selecionam um tópico para seu grupo (Sala).
+🕔 5. (5 min) Seleção do tópico que cada grupo irá estudar. Cada grupo divide seu tópico em subtópicos e dividi o trabalho dentro do grupo.
 
-🕕 6. (5 min) Cada grupo divide seu tópico em subtópicos para dividir o trabalho dentro do grupo (Sala).
+🕚 6. (30 min) Preparação das apresentações de cada um dos grupos (reunião do grupo).
 
-🕖 7. (30 min) Preparação dos subtópicos pelos membros do grupo (estudo individual).
+🕛 7. (10 min) Envio via Classroom do relatório da atividade para postagem na internet (atividade do grupo).
 
-🕗 8. (10 min) Os membros do grupo discutem o tema, como um painel de especialistas, apresentando os conceitos e questões chaves (reunião do grupo).
+🕐 8. (10 min/grupo) Apresentações de cada um dos grupos (sala).
 
-🕘 9. (10 min) Apresentação formais dos subtópicos em cada grupo (reunião do grupo).
+🕑 9. (20 min/grupo) Avaliações (sala ou extra sala).
 
-🕙 10. (5 min/grupo) Apresentações de subtópicos pelos grupos (seleção por sorteio a critério do professsor) (sala).
+🕒 10. (10 min/grupo) Feedback (sala).
 
-## Roteiro
+"""
 
-#### Momento Pós
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
 
-🕚 11. (30 min) Preparação das apresentações de cada um dos grupos (reunião do grupo).
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
 
-🕛 12. (10 min) Envio via Classroom do relatório da atividade para postagem na internet (atividade do grupo).
+julia_version = "1.7.2"
+manifest_format = "2.0"
 
-🕐 13. (10 min/grupo) Apresentações de cada um dos grupos (sala).
-
-🕑 14. (20 min/grupo) Avaliações (sala ou extra sala).
-
-🕒 15. (10 min/grupo) Feedback (sala).
-
+[deps]
 """
 
 # ╔═╡ Cell order:
 # ╟─e9af4550-b632-11eb-1d0d-e11b45468706
 # ╟─4fb54a13-3db2-4475-bd49-fd0f9d5cefa4
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
